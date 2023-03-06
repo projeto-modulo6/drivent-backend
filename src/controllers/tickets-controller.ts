eb549a84e8da49721fbd81f6980dd03dcfa13e86
MarcusCastro0701 <marcus.castro0701@gmail.com>
2023-03-06 10:57:28 -0300

    add: getTicketTypeByEnrollmentId

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -1,7 +1,21 @@
-import { AuthenticatedRequest } from "@/middlewares";
-import ticketService from "@/services/tickets-service";
-import { Response } from "express";
-import httpStatus from "http-status";
+import { AuthenticatedRequest } from '@/middlewares';
+import ticketService from '@/services/tickets-service';
+import { Response } from 'express';
+import httpStatus from 'http-status';
+
+export async function getTicketByEnrollmentId(req: AuthenticatedRequest, res: Response) {
+  const id = req.params.enrollmentId;
+
+  const enrollmentId = Number(id);
+
+  try {
+    const ticketByEnrollmentId = await ticketService.getTicketByEnrollmentId(enrollmentId);
+
+    return ticketByEnrollmentId.TicketType;
+  } catch (error) {
+    return res.sendStatus(httpStatus.NO_CONTENT);
+  }
+}
 
 export async function getTicketTypes(req: AuthenticatedRequest, res: Response) {
   try {
@@ -43,4 +57,3 @@ export async function createTicket(req: AuthenticatedRequest, res: Response) {
     return res.sendStatus(httpStatus.NOT_FOUND);
   }
 }
-
--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authenticateToken } from "@/middlewares";
-import { getTicketTypes, getTickets, createTicket } from "@/controllers";
+import { getTicketTypes, getTickets, createTicket, getTicketByEnrollmentId } from "@/controllers";
 
 const ticketsRouter = Router();
 
 ticketsRouter
   .all("/*", authenticateToken)
   .get("/types", getTicketTypes)
+  .get("/types/:id", getTicketByEnrollmentId)
   .get("", getTickets)
   .post("", createTicket);
 
