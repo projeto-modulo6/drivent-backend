0366272dfc4599fded85dc4abf825fbd870f2dd0
Lucas Macedo <lucasamacedo@hotmail.com>
2023-03-13 03:28:44 -0300

    fix: comunication between redis and postgresql on enrollments repository

--- src/config/databaseCache.ts
+++ src/config/databaseCache.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 const redis = createClient({
   url: process.env.REDIS_URL,
--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -1,11 +1,11 @@
-import { AddressEnrollment } from '@/protocols';
-import { getAddress } from '@/utils/cep-service';
-import { notFoundError } from '@/errors';
-import addressRepository, { CreateAddressParams } from '@/repositories/address-repository';
-import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
-import { exclude } from '@/utils/prisma-utils';
-import { Address, Enrollment } from '@prisma/client';
-import redis from '@/config/databaseCache';
+import { AddressEnrollment } from "@/protocols";
+import { getAddress } from "@/utils/cep-service";
+import { notFoundError } from "@/errors";
+import addressRepository, { CreateAddressParams } from "@/repositories/address-repository";
+import enrollmentRepository, { CreateEnrollmentParams } from "@/repositories/enrollment-repository";
+import { exclude } from "@/utils/prisma-utils";
+import { Address, Enrollment } from "@prisma/client";
+import redis from "@/config/databaseCache";
 
 async function getAddressFromCEP(cep: string): Promise<AddressEnrollment> {
   const result = await getAddress(cep);
@@ -38,23 +38,23 @@ async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddr
   const address = getFirstAddress(firstAddress);
 
   return {
-    ...exclude(enrollmentWithAddress, 'userId', 'createdAt', 'updatedAt', 'Address'),
+    ...exclude(enrollmentWithAddress, "userId", "createdAt", "updatedAt", "Address"),
     ...(!!address && { address }),
   };
 }
 
-type GetOneWithAddressByUserIdResult = Omit<Enrollment, 'userId' | 'createdAt' | 'updatedAt'>;
+type GetOneWithAddressByUserIdResult = Omit<Enrollment, "userId" | "createdAt" | "updatedAt">;
 
 function getFirstAddress(firstAddress: Address): GetAddressResult {
   if (!firstAddress) return null;
 
-  return exclude(firstAddress, 'createdAt', 'updatedAt', 'enrollmentId');
+  return exclude(firstAddress, "createdAt", "updatedAt", "enrollmentId");
 }
 
-type GetAddressResult = Omit<Address, 'createdAt' | 'updatedAt' | 'enrollmentId'>;
+type GetAddressResult = Omit<Address, "createdAt" | "updatedAt" | "enrollmentId">;
 
 async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollmentWithAddress) {
-  const enrollment = exclude(params, 'address');
+  const enrollment = exclude(params, "address");
   const address = getAddressForUpsert(params.address);
 
   //BUG - Verificar se o CEP é válido
@@ -64,7 +64,7 @@ async function createOrUpdateEnrollmentWithAddress(params: CreateOrUpdateEnrollm
     throw notFoundError();
   }
 
-  const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, 'userId'));
+  const newEnrollment = await enrollmentRepository.upsert(params.userId, enrollment, exclude(enrollment, "userId"));
 
   await addressRepository.upsert(newEnrollment.id, address, address);
 }
