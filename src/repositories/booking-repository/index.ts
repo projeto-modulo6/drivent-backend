13a88ca2de83f27b03b80340ee5312431a40c542
Lucas Macedo <lucasamacedo@hotmail.com>
2023-03-13 13:19:48 -0300

    fix: delete information on redis when it is updated on postgresql

--- src/repositories/booking-repository/index.ts
+++ src/repositories/booking-repository/index.ts
@@ -1,9 +1,9 @@
-import { prisma } from '@/config';
-import redis from '@/config/databaseCache';
-import { Booking, Room } from '@prisma/client';
+import { prisma } from "@/config";
+import redis from "@/config/databaseCache";
+import { Booking, Room } from "@prisma/client";
 
-type CreateParams = Omit<Booking, 'id' | 'createdAt' | 'updatedAt'>;
-type UpdateParams = Omit<Booking, 'createdAt' | 'updatedAt'>;
+type CreateParams = Omit<Booking, "id" | "createdAt" | "updatedAt">;
+type UpdateParams = Omit<Booking, "createdAt" | "updatedAt">;
 
 async function create({ roomId, userId }: CreateParams): Promise<Booking> {
   return prisma.booking.create({
@@ -45,17 +45,18 @@ async function findByUserId(userId: number) {
     },
   });
 
-  redis.setEx(`booking-userId-${userId}`, expiration, JSON.stringify(data));
+  redis.setEx(`bookingId-${data.id}-userId-${userId}`, expiration, JSON.stringify(data));
   return data;
 }
 
 async function findByUserIdCache(userId: number): Promise<Booking & { Room: Room }> {
-  const cacheBooking = await redis.get(`booking-userId-${userId}`);
+  const key = (await redis.keys(`booking*userId-${userId}`))[0];
+  const cacheBooking = await redis.get(String(key));
   return JSON.parse(cacheBooking);
 }
 
 async function upsertBooking({ id, roomId, userId }: UpdateParams) {
-  return prisma.booking.upsert({
+  const data = await prisma.booking.upsert({
     where: {
       id,
     },
@@ -67,6 +68,10 @@ async function upsertBooking({ id, roomId, userId }: UpdateParams) {
       roomId,
     },
   });
+  const key = await redis.keys(`*bookingId-${data.id}*`);
+  redis.unlink(key);
+  redis.del(`booking-roomId-${roomId}`);
+  return data;
 }
 
 const bookingRepository = {
