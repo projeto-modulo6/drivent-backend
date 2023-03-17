e7923ab2b12572a809f3d35a525e279b021ae053
Lucas Macedo <lucasamacedo@hotmail.com>
2023-03-17 02:10:11 -0300

    feat: route datesWithActivities

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -1,16 +1,50 @@
 import { prisma } from "@/config";
 
-async function findActivityById(activityId: number){
-    const data = await prisma.activity.findFirst({
-        where:{
-            id: activityId
-        }
-    });
-    return data;
+async function findActivityById(activityId: number) {
+  const data = await prisma.activity.findFirst({
+    where: {
+      id: activityId,
+    },
+  });
+  return data;
+}
+
+async function findAllDates() {
+  const data = await prisma.date_activity.findMany({
+    orderBy: {
+      date: "asc",
+    },
+  });
+  return data;
+}
+
+async function findAllLocalsWithActivity(id: number) {
+  const data = await prisma.local.findMany({
+    include: {
+      activity: {
+        include: {
+          _count: {
+            select: {
+              user_activity: true,
+            },
+          },
+        },
+        where: {
+          date_id: id,
+        },
+        orderBy: {
+          begin: "asc",
+        },
+      },
+    },
+  });
+  return data;
 }
 
 const activityRepository = {
-    findActivityById,
-}
+  findActivityById,
+  findAllDates,
+  findAllLocalsWithActivity,
+};
 
 export default activityRepository;
